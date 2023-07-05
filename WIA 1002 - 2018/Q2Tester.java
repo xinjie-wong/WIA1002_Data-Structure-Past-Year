374aaf6bb517067bb8f4deeee1fc0e8834287fba
Xin Jie W <116251172+xinjie-wong@users.noreply.github.com>
2023-07-05 12:27:14 +0800

    Add files via upload

---
+++ WIA 1002 - 2018/Q2Tester.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package WIA2018;
+
+/**
+ *
+ * @author Xin Jie
+ */
+public class Q2Tester {
+    public static void main(String[] args) {
+        Q2Queue q = new Q2Queue(5);
+        
+        q.enqueue("hello");
+        q.enqueue("all");
+        q.enqueue("who");
+        q.enqueue("wants");
+        q.enqueue("ice cream");
+        q.dequeue();
+        q.dequeue();
+        q.enqueue("1000");
+        q.enqueue("2000");
+        q.peek();
+        
+        q.display();
+    }
+    
+}
